833c2a942ab8304101fc3d69aa5a83da2002f6c3
Vasu Srinivasan <vasusrinvsn@gmail.com>
2015-03-13 22:23:15 -0700

    I'M DONE

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,28 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all # Get's all the dogs in our database and assigns it the the instance variable @dog.
+  end
+
+  def new
+    @dog = Dog.new #Creates an empty dog object.
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    if @dog.save #If saving the dog was successful
+      redirect_to @dog #Go to the show view of the dog
+    else
+      render "new" #Go to the new view for the dog
+    end
+  end
+
+  def show
+    @dog = Dog.find(params[:id]) # Query for the dog with the id that is in params
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :email) #Returns a hash that was the value of "name" and "email" from the value of "dog" in params.
+  end
+end
